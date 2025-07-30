88abc128105d5522f11ffe1f4d10a2b6f7ea9bd3
Dasyel Willems <d.willems@tbauctions.com>
2025-07-30 23:46:53 +0200

    add zoom with [ and ]

--- camera_2d.gd
+++ camera_2d.gd
@@ -8,9 +8,12 @@ func _process(delta):
 		Input.get_action_strength("camera_move_down") - Input.get_action_strength("camera_move_up")
 	)
 	
+	var zoom_value := Input.get_action_strength("camera_zoom_in") - Input.get_action_strength("camera_zoom_out")
+	
 	
 	# Normalize to prevent faster diagonal movement
 	if input_vector.length_squared() > 1.0:
 		input_vector = input_vector.normalized()
 
 	position += input_vector * move_speed * delta
+	zoom += Vector2(zoom_value, zoom_value) * delta
