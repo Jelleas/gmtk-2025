24c9eebf48b99314bb4bf59747fcda17888d85ae
jelleas <jelle.van.assema@gmail.com>
2025-07-30 23:12:39 +0200

    rm dead code

--- resource.gd
+++ resource.gd
@@ -1,9 +1,7 @@
 extends PathFollow2D
 
-@export var loop_seconds: float = 10
 var content = null
 #
 func init(content_, progress_ratio_: float, loop_seconds_: float):
-	loop_seconds = loop_seconds_
 	progress_ratio = progress_ratio_
 	content = content_
