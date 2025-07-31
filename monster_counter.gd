697f57edbbe6b543ccf6ea64350c86457f3f5d68
jelleas <jelle.van.assema@gmail.com>
2025-07-31 17:23:04 +0200

    escaped counter

---
+++ monster_counter.gd
@@ -0,0 +1,18 @@
+extends Label
+
+var escaped_monsters_count = 0
+
+var monster_path: Path2D
+var initialized = false
+
+func _ready() -> void:
+	add_to_group("monster_trackers")
+
+func init(monster_path_: Path2D) -> void:
+	monster_path = monster_path_
+	initialized = true
+	monster_path.monster_escape.connect(_on_monster_escape)
+
+func _on_monster_escape(monster: Monster):
+	escaped_monsters_count += 1
+	text = str(escaped_monsters_count) + " escaped"
