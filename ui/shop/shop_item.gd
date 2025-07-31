092e5cf8e93f03acffcbd6300da252abcb48fced
jelleas <jelle.van.assema@gmail.com>
2025-07-31 20:38:11 +0200

    better shop

--- ui/shop/shop_item.gd
+++ ui/shop/shop_item.gd
@@ -8,6 +8,7 @@ var item # TODO type hint
 
 func _ready() -> void:
 	$Button.custom_minimum_size = Vector2(100, 50)
+	$Button.button_up.connect(_on_button_up)
 
 func init(item_) -> void: # TODO type hint
 	item = item_
