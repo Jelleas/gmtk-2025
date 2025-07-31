cebfc734ff728d5f3bee3a5be836b53ab03627d6
jelleas <jelle.van.assema@gmail.com>
2025-07-31 20:37:14 +0200

    better shop

--- ui/shop/shop_item.gd
+++ ui/shop/shop_item.gd
@@ -7,11 +7,12 @@ signal item_bought(item) # TODO type hint
 var item # TODO type hint
 
 func _ready() -> void:
-	$Button.button_up.connect(_on_button_up)
+	$Button.custom_minimum_size = Vector2(100, 50)
 
 func init(item_) -> void: # TODO type hint
 	item = item_
-	$Label.text = item
+	$GridContainer/NameLabel.text = item
+	$GridContainer/PriceLabel.text = "100$"
 
 func _on_button_up() -> void:
 	if item != null:
