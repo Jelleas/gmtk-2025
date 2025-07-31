f58193257157b99e392d6678a0120d047a5df0d0
WietzeS <wietze@kryha.io>
2025-07-31 16:38:21 +0200

    tower rename to totem

--- ui/create_consumer.gd
+++ ui/create_consumer.gd
@@ -1,20 +1,20 @@
 extends Button
 
-var tower_scene: PackedScene
+var totem_scene: PackedScene
 var resource_manager: Path2D
 var start: int
 var end: int
 
-func init(res_man: Path2D, tower: PackedScene, position_x: int, position_y: int):
+func init(res_man: Path2D, totem: PackedScene, position_x: int, position_y: int):
 	resource_manager = res_man
-	tower_scene = tower
+	totem_scene = totem
 	start = position_x
 	end = position_y
 	
 func _pressed() -> void:
-	var tower = tower_scene.instantiate()
-	add_child(tower)
+	var totem = totem_scene.instantiate()
+	add_child(totem)
 	
 	# init resource script
-	tower.init(resource_manager, -1, start, end)
-	tower.add_upgrade()
+	totem.init(resource_manager, -1, start, end)
+	totem.add_upgrade()
--- ui/totem_interface.gd
+++ ui/totem_interface.gd
@@ -1,27 +1,27 @@
 extends Control
 
 @export var resource_manager: Path2D
-@export var tower_scene: PackedScene
+@export var totem_scene: PackedScene
 
 func _ready() -> void:
-	$CreateProducers/CreateProducer1.init(resource_manager, tower_scene, 51, 52)
-	$CreateProducers/CreateProducer2.init(resource_manager, tower_scene, 47, 48)
-	$CreateProducers/CreateProducer3.init(resource_manager, tower_scene, 43, 44)
-	$CreateProducers/CreateProducer4.init(resource_manager, tower_scene, 39, 40)
+	$CreateProducers/CreateProducer1.init(resource_manager, totem_scene, 51, 52)
+	$CreateProducers/CreateProducer2.init(resource_manager, totem_scene, 47, 48)
+	$CreateProducers/CreateProducer3.init(resource_manager, totem_scene, 43, 44)
+	$CreateProducers/CreateProducer4.init(resource_manager, totem_scene, 39, 40)
 	
-	$CreateConsumers/CreateConsumer1.init(resource_manager, tower_scene, 0, 1)
-	$CreateConsumers/CreateConsumer2.init(resource_manager, tower_scene, 3, 4)
-	$CreateConsumers/CreateConsumer3.init(resource_manager, tower_scene, 9, 10)
-	$CreateConsumers/CreateConsumer4.init(resource_manager, tower_scene, 15, 15)
-	$CreateConsumers/CreateConsumer5.init(resource_manager, tower_scene, 18, 19)
-	$CreateConsumers/CreateConsumer6.init(resource_manager, tower_scene, 21, 22)
-	$CreateConsumers/CreateConsumer7.init(resource_manager, tower_scene, 27, 28)
-	$CreateConsumers/CreateConsumer8.init(resource_manager, tower_scene, 33, 34)
-	$CreateConsumers/CreateConsumer9.init(resource_manager, tower_scene, 36, 37)
-	$CreateConsumers/CreateConsumer10.init(resource_manager, tower_scene, 39, 40)
-	$CreateConsumers/CreateConsumer11.init(resource_manager, tower_scene, 45, 46)
-	$CreateConsumers/CreateConsumer12.init(resource_manager, tower_scene, 51, 52)
-	$CreateConsumers/CreateConsumer13.init(resource_manager, tower_scene, 54, 55)
-	$CreateConsumers/CreateConsumer14.init(resource_manager, tower_scene, 57, 58)
-	$CreateConsumers/CreateConsumer15.init(resource_manager, tower_scene, 63, 64)
-	$CreateConsumers/CreateConsumer16.init(resource_manager, tower_scene, 69, 70)
+	$CreateConsumers/CreateConsumer1.init(resource_manager, totem_scene, 0, 1)
+	$CreateConsumers/CreateConsumer2.init(resource_manager, totem_scene, 3, 4)
+	$CreateConsumers/CreateConsumer3.init(resource_manager, totem_scene, 9, 10)
+	$CreateConsumers/CreateConsumer4.init(resource_manager, totem_scene, 15, 15)
+	$CreateConsumers/CreateConsumer5.init(resource_manager, totem_scene, 18, 19)
+	$CreateConsumers/CreateConsumer6.init(resource_manager, totem_scene, 21, 22)
+	$CreateConsumers/CreateConsumer7.init(resource_manager, totem_scene, 27, 28)
+	$CreateConsumers/CreateConsumer8.init(resource_manager, totem_scene, 33, 34)
+	$CreateConsumers/CreateConsumer9.init(resource_manager, totem_scene, 36, 37)
+	$CreateConsumers/CreateConsumer10.init(resource_manager, totem_scene, 39, 40)
+	$CreateConsumers/CreateConsumer11.init(resource_manager, totem_scene, 45, 46)
+	$CreateConsumers/CreateConsumer12.init(resource_manager, totem_scene, 51, 52)
+	$CreateConsumers/CreateConsumer13.init(resource_manager, totem_scene, 54, 55)
+	$CreateConsumers/CreateConsumer14.init(resource_manager, totem_scene, 57, 58)
+	$CreateConsumers/CreateConsumer15.init(resource_manager, totem_scene, 63, 64)
+	$CreateConsumers/CreateConsumer16.init(resource_manager, totem_scene, 69, 70)
