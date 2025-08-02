e1b5b8a0fba6ab95636fcefbf34b44e9aeed1fa0
Dasyel <dasyelwillems@gmail.com>
2025-08-02 11:05:20 +0200

    Added bigger tiles and fixed zoom

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,6 +1,9 @@
 extends Camera2D
 
 @export var move_speed := 300.0
+@export var mouse_zoom_speed := 0.1
+@export var min_zoom_value := 0.5
+@export var max_zoom_value := 3
 
 func _process(delta):
 	var input_vector := Vector2(
@@ -16,4 +19,15 @@ func _process(delta):
 		input_vector = input_vector.normalized()
 
 	position += input_vector * move_speed * delta
-	zoom += Vector2(zoom_value, zoom_value) * delta
+	_zoom(zoom_value * delta)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			_zoom(mouse_zoom_speed)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			_zoom(-mouse_zoom_speed)
+
+func _zoom(value: float):
+	var zoomFactor = clampf(zoom.x + value, min_zoom_value, max_zoom_value)
+	zoom = Vector2(zoomFactor, zoomFactor)
