67ec9ed87ea7c1d86d19d37a91d75cf88ef7333f
Dasyel Willems <d.willems@tbauctions.com>
2025-08-02 22:08:52 +0200

    static resource image function

--- resource.gd
+++ resource.gd
@@ -15,7 +15,7 @@ enum Type {
 	WOOD
 }
 
-static func type_to_image_path(_type: Res.Type) -> String:
+static func image_path(_type: Res.Type) -> String:
 	match _type:
 		Type.BERRY_JUICE: return "res://assets/resources/berry-juice.png"
 		Type.BERRY: return "res://assets/resources/berry.png"
@@ -35,5 +35,5 @@ var type: Res.Type
 func init(type_: Res.Type, progress_ratio_: float):
 	progress_ratio = progress_ratio_
 	type = type_
-	sprite.texture = load(type_to_image_path(type))
+	sprite.texture = load(image_path(type))
 	
