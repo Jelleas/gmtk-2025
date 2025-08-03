aafb644e4b1c698c876986ab01c601a600b69c0b
jelleas <jelle.van.assema@gmail.com>
2025-08-03 16:17:56 +0200

    recipe tracker

--- totem/recipe_container.gd
+++ totem/recipe_container.gd
@@ -4,12 +4,16 @@ var totem: Totem
 	
 func _ready() -> void:
 	$HBoxContainer/ArrowRect.hide()
+	$HBoxContainer/ArrowRect.texture = get_image(load("res://assets/resources/arrow.png"))
 	hide()
 	
 func init(totem_: Totem) -> void:
 	totem = totem_
 	totem.TotemChanged.connect(_on_totem_changed)
 	show()
+	set_conversion_arrow()
+	set_consumes()
+	set_produces()
 
 func _on_totem_changed(totem_: Totem) -> void:
 	totem = totem_
@@ -20,7 +24,10 @@ func _on_totem_changed(totem_: Totem) -> void:
 	set_produces()
 	
 func set_conversion_arrow() -> void:
-	if totem.get_consumes().size() > 0 and totem.get_produces().size() > 0:
+	var is_consuming = totem.get_consumes().size() > 0
+	var is_producing = totem.get_produces().size() > 0 or totem.get_shoots_image_path() != null
+	
+	if is_consuming and is_producing:
 		$HBoxContainer/ArrowRect.show()
 	else:
 		$HBoxContainer/ArrowRect.hide()
@@ -31,21 +38,40 @@ func set_consumes() -> void:
 	for child in container.get_children():
 		child.free()
 	
-	set_resources_in_container(
-		totem.get_consumes(),
-		container
-	)
-
+	var consumes = totem.get_consumes()
+	
+	if consumes.size() == 0:
+		$HBoxContainer/ConsumesContainer.hide()
+	else:
+		set_resources_in_container(
+			consumes,
+			container
+		)
+		$HBoxContainer/ConsumesContainer.show()
+	
 func set_produces() -> void:
 	var container = $HBoxContainer/ProducesContainer/MarginContainer/HBoxContainer
 		
 	for child in container.get_children():
 		child.free()
 	
-	set_resources_in_container(
-		totem.get_produces(),
-		container
-	)
+	var produces = totem.get_produces()
+	print(produces)
+	if produces.size() == 0 and totem.get_shoots_image_path() == null:
+		$HBoxContainer/ProducesContainer.hide()
+	else:
+		set_resources_in_container(
+			produces,
+			container
+		)
+		$HBoxContainer/ProducesContainer.show()
+		
+		# add shooting items to produces
+		if totem.get_shoots_image_path() != null:
+			var rect = TextureRect.new()
+			var shoots_image_path = totem.get_shoots_image_path()
+			rect.texture = get_image(load(shoots_image_path))
+			container.add_child(rect)
 		
 func set_resources_in_container(resources: Array[Res.Type], container: Container) -> void:
 	for r in resources:
