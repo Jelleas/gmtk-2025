7215df876c7069e33b52912551565533df922f9b
jelleas <jelle.van.assema@gmail.com>
2025-08-03 15:18:58 +0200

    when in doubt, panel

--- totem/recipe_container.gd
+++ totem/recipe_container.gd
@@ -11,20 +11,22 @@ func init(totem_: Totem) -> void:
 	totem.TotemChanged.connect(_on_totem_changed)
 	show()
 
-func _on_totem_changed(totem_: Totem):
+func _on_totem_changed(totem_: Totem) -> void:
 	totem = totem_
 	show()
 	
+	set_conversion_arrow()
+	set_consumes()
+	set_produces()
+	
+func set_conversion_arrow() -> void:
 	if totem.get_consumes().size() > 0 and totem.get_produces().size() > 0:
 		$HBoxContainer/ArrowRect.show()
 	else:
 		$HBoxContainer/ArrowRect.hide()
-	
-	set_consumes()
-	set_produces()
-	
-func set_consumes():
-	var container = $HBoxContainer/ConsumesContainer
+
+func set_consumes() -> void:
+	var container = $HBoxContainer/ConsumesContainer/MarginContainer/HBoxContainer
 	
 	for child in container.get_children():
 		child.free()
@@ -34,8 +36,8 @@ func set_consumes():
 		container
 	)
 
-func set_produces():
-	var container = $HBoxContainer/ProducesContainer
+func set_produces() -> void:
+	var container = $HBoxContainer/ProducesContainer/MarginContainer/HBoxContainer
 		
 	for child in container.get_children():
 		child.free()
@@ -54,5 +56,5 @@ func set_resources_in_container(resources: Array[Res.Type], container: Container
 
 func get_image(resource: Resource) -> ImageTexture:
 	var image = resource.get_image()
-	image.resize(32, 32, Image.INTERPOLATE_LANCZOS)
+	image.resize(48, 48, Image.INTERPOLATE_LANCZOS)
 	return ImageTexture.create_from_image(image)
